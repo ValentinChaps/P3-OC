03a0fb948a7cf9f8849c2d0fc7136d9deec17c1c
Valentin Chapel <109554149+ValentinChaps@users.noreply.github.com>
2023-07-15 18:07:44 +0200

    Installation, mise en place et intégration des données de l'API pour les projets

---
+++ FrontEnd/Projets.js
@@ -0,0 +1,22 @@
+const reponse = await fetch('http://localhost:5678/api/works');
+const projets = await reponse.json();
+
+function genererProjets(projets){
+    for (let i = 0; i < projets.length; i++){
+        
+        const article = projets[i]
+
+        const sectionGallery = document.querySelector(".gallery")
+        const projetElement = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        imageElement.src = article.imageUrl
+        const titreElement = document.createElement("figcaption")
+        titreElement.innerText = article.title
+
+        sectionGallery.appendChild(projetElement)
+        projetElement.appendChild(imageElement)
+        projetElement.appendChild(titreElement)
+    }
+}
+
+genererProjets(projets)
