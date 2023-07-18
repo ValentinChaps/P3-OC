0801117a136820a5c6823f5eeb646303319c7d27
Valentin Chapel <109554149+ValentinChaps@users.noreply.github.com>
2023-07-18 18:00:26 +0200

    Intégration du token dans local storage après connexion puis redirection sur la page d'accueil. 1er ébauche de la barre du menu édition

--- FrontEnd/seConnecter.js
+++ FrontEnd/seConnecter.js
@@ -1,10 +1,30 @@
 const inputEmail = document.querySelector("#e-mail")
 const inputMotDePasse = document.querySelector("#motDePasse")
 const boutonSeConnecter = document.querySelector("#seConnecter")
-boutonSeConnecter.addEventListener("click", function (){
+let token = ""
+boutonSeConnecter.addEventListener("click", function (event){
     if (inputEmail.value ==="sophie.bluel@test.tld" && inputMotDePasse.value ==="S0phie"){
-        const connecter = "Connecter"
-        console.log(connecter)
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              email: inputEmail.value,
+              password: inputMotDePasse.value
+            })
+          })
+            .then(response => response.json())
+            .then(data => {
+              token = data.token
+              localStorage.setItem("token", token)
+              event.preventDefault
+              window.location.href = "index.html"
+              console.log(token)
+            })
+            .catch(error => {
+              console.log("Une erreur s'est produite :", error)
+            })
     }
     else{
         const erreur = document.querySelector("#messageErreur")
